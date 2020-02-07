22d73b3d5cc586959b2eb68b65aa322af3748aae
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 20:45:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
-  football_teams =>
-	{:Leagues => {}
+  football_teams ={
+		:Leagues => {}
 
 	}# Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
