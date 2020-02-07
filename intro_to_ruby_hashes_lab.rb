b8eae18a1b26b5f6586db94a53265a3362abe919
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 21:28:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 		:railroads => {
-		:names => {:reading_railroad => {}, :pennsylvania_railroad => {}, "California" => {}, "Puerto Rico" => {}},
+		:names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, "Puerto Rico" => {}},
 		:rent_in_dollars => {
 			:one_piece_owned => 25, :two_pieces_owned => 50 , :three_pieces_owned => 100, :four_pieces_owned => 200
 		},
